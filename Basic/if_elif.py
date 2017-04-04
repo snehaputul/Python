7cb998360e1a221c5a8b902d4d4880bdff0f49d5
Sneha Paul <snehapaulputul@gmail.com>
2017-04-04 10:06:32 +0600

    if

--- Basic/if_elif.py
+++ Basic/if_elif.py
@@ -1,11 +1,11 @@
-x =int( input("Please enter an integer: "))
+x=int(input("pls enter a number :"))
 
-if x < 0:
-    x = 0
-    print('Negative changed to zero')
-elif x == 0:
-    print('Zero')
-elif x == 1:
-    print('Single')
-else:
-    print('More')
+if x<0:
+    x==0
+    print('negative to zero')
+elif x==0:
+    print('zero')
+elif x==1:
+    print('single')
+else
+    print('more')
