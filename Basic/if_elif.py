4d85f60f14b6ed520cbe91e0a957bdf5af458dc2
Sneha Paul <snehapaulputul@gmail.com>
2017-04-07 11:31:21 +0600

    if

--- Basic/if_elif.py
+++ Basic/if_elif.py
@@ -1,11 +1,11 @@
-x=int(input("pls enter a number :"))
+x=int(input('enter a number\n'))
 
 if x<0:
-    x==0
-    print('negative to zero')
+    x=0
+    print('negative turn to zero')
 elif x==0:
     print('zero')
 elif x==1:
     print('single')
-else
+else:
     print('more')
